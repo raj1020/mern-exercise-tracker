01bc31f1ea2b8d63cbbb5882eb59c7f7ce320dd9
raj1020 <raj.1020@hotmail.com>
2020-02-22 22:52:49 +1100

    added and modified codes in the models

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
 
-const userSchema = new userSchema({
+consyt Schema = mongoose.Schema;
+
+const userSchema = new Schema({
     username: {
         type: String,
         required: true,
         unique: true,
-        trim: true,
+        trim: true, //this trims whitespaces after string. For eg. if someone types whitespaces then they are trimmed off. 
         minlength: 3
     },
 }, {
     timestamps: true,
 } );
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+
+
